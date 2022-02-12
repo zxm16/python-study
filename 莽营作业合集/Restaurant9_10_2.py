b1d8d5b53a4b7230255281f50d5ce648fe085e7a
张吃饭 <91314089+zxm16@users.noreply.github.com>
2022-02-13 01:47:04 +0800

    Create Restaurant9_10_2.py

---
+++ 莽营作业合集/Restaurant9_10_2.py
@@ -0,0 +1,8 @@
+from Restaurant9_10_1 import Restaurant
+my_restaurant = Restaurant('熊家里', '熊餐')
+# 给类里的方法赋值
+my_restaurant.describe_restaurant()
+# 通过‘.’来调用describe的值
+my_restaurant.open_restaurant()
+# 通过‘.’来调用open的值
+my_restaurant.user_want()
