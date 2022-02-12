e689c8461d0bb50efe88dddbd29806813b39b2e3
张吃饭 <91314089+zxm16@users.noreply.github.com>
2022-02-13 01:48:58 +0800

    Create model9_12_1.py

---
+++ 莽营作业合集/model9_12_1.py
@@ -0,0 +1,8 @@
+class UserPower:
+
+    def __init__(self, firstname, lastname):
+        self.FirstName = firstname
+        self.LastName = lastname
+
+    def write_hello_user(self):
+        print(f"Hello{self.FirstName}{self.LastName}! welcome to the python world!")
