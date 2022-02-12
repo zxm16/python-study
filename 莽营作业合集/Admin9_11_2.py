c82b47e607faee1e4b345b076f1ba4d5beda2d7c
张吃饭 <91314089+zxm16@users.noreply.github.com>
2022-02-13 01:48:26 +0800

    Create Admin9_11_2.py

---
+++ 莽营作业合集/Admin9_11_2.py
@@ -0,0 +1,13 @@
+from Admin9_11_1 import AdminPower
+
+AdminPower01 = AdminPower('张', '鑫满')
+AdminPower01.print_hello_admin()
+
+print('---小题分界线---')
+AdminPower01.adminer.admin_name('你可以添加这个')
+AdminPower01.adminer.admin_name01()
+AdminPower01.adminer.admin_name('你可以删除这个')
+AdminPower01.adminer.admin_name01()
+AdminPower01.adminer.admin_name('你可以移动那个')
+AdminPower01.adminer.admin_name01()
+AdminPower01.adminer.admin_name03()
