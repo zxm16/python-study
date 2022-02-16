f0af408488f15dcd7429141eb8d713d17b30c578
张吃饭 <91314089+zxm16@users.noreply.github.com>
2022-02-16 10:06:19 +0800

    Create alien_invasion_setting.py

---
+++ 外星人大战游戏项目/alien_invasion_setting.py
@@ -0,0 +1,10 @@
+class Setting:
+    """
+    储存游戏中所有设置的类
+    """
+    def __init__(self):
+        # 初始化游戏设置
+        # 设置屏幕样式
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
