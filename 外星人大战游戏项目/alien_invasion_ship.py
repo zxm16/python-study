c153a9b1e12aafa080b713046bd87dc85e1b30cf
张吃饭 <91314089+zxm16@users.noreply.github.com>
2022-02-17 10:03:32 +0800

    Update alien_invasion_ship.py

--- 外星人大战游戏项目/alien_invasion_ship.py
+++ 外星人大战游戏项目/alien_invasion_ship.py
@@ -7,6 +7,10 @@ class Ship:
 
         self.screen = ai_game.screen
         # 将屏幕赋值给self.screen
+
+        self.setting = ai_game.settings
+        # 添加一个设置链接设置模块来控制飞船移动距离
+
         self.screen_rect = ai_game.screen.get_rect()
         # 访问屏幕的rect并且其值赋给self.screen_rect
 
@@ -20,6 +24,35 @@ class Ship:
 
         self.rect.midbottom = self.screen_rect.midbottom
         # 让每艘新飞船都在屏幕中央
+
+        self.x = float(self.rect.x)
+        # 在飞船x中储蓄最小值
+
+        self.moving_right = False
+        # 向右移动标志
+
+        self.moving_left = False
+        # 向左移动标志
+
+    def update(self):
+        """
+        向左向右移动飞船并且设置其速度
+        及固定左右边使其到边停止
+        """
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            # 当移动数值小于屏幕的长度时可以一直移动。
+
+            self.x += self.setting.ship_speed
+            # 在基础移动距离的情况下一直增加（累加）ship_speed这样速度会越来越快
+
+        if self.moving_left and self.rect.left > 0:
+            # 当x值大于0时可以一直向左运动
+
+            self.x -= self.setting.ship_speed
+            # 在基础移动距离的情况下一直减少（递减）ship_speed这样速度会先减少再一直往反方向增加
+
+        self.rect.x = self.x
+        # 更新飞船在x轴的位置
 
     def blitme(self):
         """
