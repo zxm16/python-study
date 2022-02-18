d33a5d9a278b82a859228290587cc747c77ec3bf
张吃饭 <91314089+zxm16@users.noreply.github.com>
2022-02-18 08:44:10 +0800

    Update alien_invasion_setting.py

--- 外星人大战游戏项目/alien_invasion_setting.py
+++ 外星人大战游戏项目/alien_invasion_setting.py
@@ -6,8 +6,25 @@ class Setting:
         # 初始化游戏设置
         # 设置屏幕样式
         self.screen_width = 1200
+        # 设置屏幕的宽度
+
         self.screen_height = 800
+        # 设置屏幕的高度
+
         self.bg_color = (230, 230, 230)
+        # 设置屏幕的背景颜色
 
         self.ship_speed = 1.5
         # 飞船设置
+
+        self.bullet_speed = 1.0
+        # 设置子弹的速度
+
+        self.bullet_width = 3
+        # 设置子弹的宽度
+
+        self.bullet_height = 15
+        # 设置子弹的高度
+
+        self.bullet_color = (60, 60, 60)
+        # 设置子弹的底色
