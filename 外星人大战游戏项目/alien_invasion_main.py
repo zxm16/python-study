bba53837f0814ce70680687c09bb0dc012bcaf16
张吃饭 <91314089+zxm16@users.noreply.github.com>
2022-02-23 20:36:57 +0800

    Update alien_invasion_main.py

--- 外星人大战游戏项目/alien_invasion_main.py
+++ 外星人大战游戏项目/alien_invasion_main.py
@@ -27,6 +27,10 @@ class AlienInversion:
 
         self.settings = Setting()
         # 将Setting实例赋给self.settings来调用Setting
+        
+        # self.screen = pygame.display.set_mode((0, 0,), pygame.FULLSCREEN)
+        # self.settings.screen_width = self.screen.get_rect().width
+        # self.settings.screen_height = self.screen.get_rect().height
 
         self.ship = Ship(self)
         # 将Ship实例赋给self.ship来调用Ship
